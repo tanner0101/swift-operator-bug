44ea11f185d9abf08ad2834e589f2432f91f0643
tanner0101 <me@tanner.xyz>
2020-02-05 14:58:07 -0500

    rm tests

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import swift_operator_bugTests
-
-var tests = [XCTestCaseEntry]()
-tests += swift_operator_bugTests.allTests()
-XCTMain(tests)
--- Tests/swift-operator-bugTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(swift_operator_bugTests.allTests),
-    ]
-}
-#endif
